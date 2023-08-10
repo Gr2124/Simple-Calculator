d05071ae9e939378cbcb54b9e69ac3b84172de8a
dpk210 <141372079+dpk210@users.noreply.github.com>
2023-08-10 19:58:42 -0400

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -5,8 +5,13 @@ def add(x , y):
     return x + y
 
 def subtract(x, y):
-    return x - y  
-
+    return x - y 
+    
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
 
 print("Select operation:")
 print("1. Add")
